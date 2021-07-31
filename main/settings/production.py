2e9cd6277c6895cb658545703b4d200a681372e9
Ajtemir <ajtemirulanbekuulu@gmail.com>
2021-07-31 23:20:01 +0600

    deploy

--- main/settings/production.py
+++ main/settings/production.py
@@ -61,7 +61,7 @@ EMAIL_USE_SSL = False
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6exanlg@bb8((&1v=toc7r#c^2)4%dmx350*r8=t=(8hmzeq!6'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
